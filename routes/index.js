a990c05ad4044b0b4a682df52aa57d96efa59c88
Sudit Kumar Jha <jsudit2@gmail.com>
2024-03-16 18:57:37 +0530

    Practice Authentication Passport, Passport-local(Local Strategy), passport-local-mongoose(encryption / decryption)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require('./users')
+const passport = require('passport')
+// Importing passportlocal Strategy
+const localStrategy = require('passport-local')
+passport.use(new localStrategy(userModel.authenticate()))
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index');
 });
+
+
+
 
 router.get('/create', async function (req, res) {
   const createdUser = await userModel.create({
@@ -33,5 +40,50 @@ router.get('/deleteUser', async function (req, res) {
   })
   res.send(deletedUser)
 })
+
+router.get('/profile', isLoggedIn, function (req, res) {
+  res.render('profile')
+})
+
+// Register Route
+router.post('/register', function (req, res) {
+  const userData = new userModel({
+    username: req.body.username,
+    email: req.body.email
+  })
+  userModel.register(userData, req.body.password)
+    .then(function (registereduser) {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/profile')
+      })
+    })
+})
+
+// Login route
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/profile',
+  failureRedirect: '/'
+}), function (req, res) { })
+
+
+// Logout
+
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err)
+    }
+    res.redirect('/')
+  })
+})
+
+// Is logged in middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/')
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/practice')
+const plm = require('passport-local-mongoose')
+
+// Establishing Connection
+mongoose.connect('mongodb://127.0.0.1:27017/auth')
 
 const userSchema = mongoose.Schema({
-  name: String,
-  age: Number
+  username: String,
+  email: String,
+  password: String,
 })
+
+userSchema.plugin(plm)
 
 module.exports = mongoose.model("user", userSchema)
